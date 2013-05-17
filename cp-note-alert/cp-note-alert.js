24ba3836e67229f67cb91b2e79c5f8a589a39759
danlenharris <daniel.harris@opensquare.co.uk>
2013-05-17 17:00:39 +0100

    CP - User alert js tweak

--- cp-note-alert/cp-note-alert.js
+++ cp-note-alert/cp-note-alert.js
@@ -1,7 +1,8 @@
 function Widget_cp_note_alert() {
 
 	this.onReadyExtend = function() {
-		
+		//$('#alertContent').append('Grumpy wizards make toxic brew for the evil Queen and Jack.!');
+
 		var url = document.URL;
 		var policyId = url.substring(url.lastIndexOf("/") + 1);
      	policyId = policyId.substring(policyId.lastIndexOf("#") + 1);
@@ -16,17 +17,14 @@ function Widget_cp_note_alert() {
 			window.location.replace(url);
 		});
 
-		$.ajax(	{url: "proxy/scribe/notes" + '?key=' + policyId,
+		$.ajax(	{url: "proxy/scribe/notes" + '?key=' + policyId +'&group=ALERT',
           type: 'GET',
           success: function(notesArray, status, xhr) {
           	var $userAlert = ""; 
           	var alertId;        
 	          for (var i = 0; i < notesArray.length; i++) {
-
-			      if(notesArray[i].group === "ALERT"){
 			      	$userAlert = $userAlert + notesArray[i].message
 			      	alertId = notesArray[i].id;
-			      }
 			  }
 
 			 if(notesArray.length != 0){
