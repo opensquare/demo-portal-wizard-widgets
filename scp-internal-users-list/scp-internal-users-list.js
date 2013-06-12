7d6ee03a46f172cc585a280da58444d3c1a1a5c2
John Pang <john@pijin.co.uk>
2013-06-12 14:30:05 +0100

    SCP - Widget for adding a new SCP user.

--- scp-internal-users-list/scp-internal-users-list.js
+++ scp-internal-users-list/scp-internal-users-list.js
@@ -2,15 +2,23 @@ function Widget_scp_internal_users_list() {
 
 	var _this = this;
 	var channelUsersLoaded = 'scp-internal-users.users-loaded';
+	var channelUserSelected = 'scp-internal-users-profile.user-selected';
 
 	this.onReadyExtend = function() {
 		pw.addListenerToChannel(this, channelUsersLoaded);
+		pw.addListenerToChannel(this, channelUserSelected);
 		_this.$ul = $('ul', _this.$widgetDiv);
 
 		$('.usersList', _this.$widgetDiv).on('change', 'input[type=radio]', function() {
 			var userId = $(this).closest('li').data('user-id');
 			var selectedUser = getUser(userId);
-			pw.notifyChannelOfEvent('scp-internal-users-profile.user-selected', {user: selectedUser});
+			pw.notifyChannelOfEvent(channelUserSelected, {user: selectedUser});
+		});
+
+		$('.add-user-button', _this.$widgetDiv).click(function() {
+			$("#modalPopupContent").html("<div class='widget' name='scp-internal-users-add'></div>");
+			pw.mount($("#modalPopupContent .widget:first"));
+			$("#modalPopupContainer").show();
 		});
 	};
 
@@ -18,6 +26,13 @@ function Widget_scp_internal_users_list() {
 		if (channel === channelUsersLoaded) {
 			_this.users = event.users;
 			displayUsers(event.users);
+		} else if (channel === channelUserSelected) {
+			$('li', _this.$widgetDiv).each(function() {
+				var $this = $(this);
+				if ($this.data('user-id') == event.user.id) {
+					$this.find('input[type=radio]').click();
+				}
+			});
 		}
 	};
 
