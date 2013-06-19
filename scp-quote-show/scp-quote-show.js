26a63a31781da7a650c0f7c1e2963377cae2d6b4
danlenharris <daniel.harris@opensquare.co.uk>
2013-06-19 10:14:24 +0100

    SCP -quote show + form

--- scp-quote-show/scp-quote-show.js
+++ scp-quote-show/scp-quote-show.js
@@ -4,6 +4,8 @@ function Widget_scp_quote_show() {
 		var pageId = this.$widgetDiv.parents('article').attr('pageId');
 		var calcId = pageId.split('/')[2];
 		pw.notifyChannelOfEvent('scp-page.setPageTitles', { pageId: pageId, title: 'Quote Show', subTitle: 'Calc ' + calcId });
+
+		$('.widget[name="quote-forms"]', this.$widgetDiv).attr('calcref', calcId);
 	}
 
 	this.onReadyExtend = function() {
